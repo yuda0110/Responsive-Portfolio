53f9fcdd41ef36f676765c1071ed0fdfdbb09424
Yuko Uda <yuko.uda0110@gmail.com>
2020-02-21 23:19:41 -0800

    Add active class and function to the navigation's links

--- assets/js/header-footer.js
+++ assets/js/header-footer.js
@@ -9,15 +9,24 @@ const createHeader = () => {
             <div class="name">Yuko Uda</div>
             <nav>
                 <ul class="navigation">
-                    <li><a href="./index.html">About</a></li>
-                    <li><a href="./portfolio.html">Portfolio</a></li>
-                    <li><a href="./contact.html">Contact</a></li>
+                    <li><a class="nav-link" href="./index.html">About</a></li>
+                    <li><a class="nav-link" href="./portfolio.html">Portfolio</a></li>
+                    <li><a class="nav-link" href="./contact.html">Contact</a></li>
                 </ul>
             </nav>
         </div>`
   if (header) {
     header.innerHTML = headerHtml
+    activeNavLink('.nav-link')
   }
+}
+
+const activeNavLink = (navLinkClass) => {
+  document.querySelectorAll(navLinkClass).forEach((navLink) => {
+    if (location.href.includes(navLink.getAttribute('href').split("/").pop())) {
+      navLink.classList.add('active')
+    }
+  })
 }
 
 const createFooter = () => {
